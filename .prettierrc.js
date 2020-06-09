625ed14db147af99f5c69526de42084bd7f24033
dmitry <dmitry.kuznetsov.dev@gmail.com>
2020-06-09 18:42:56 +0300

    add dev tools

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  semi: true,
+};
